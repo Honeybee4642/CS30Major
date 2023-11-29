d0bf98ffd2d0e3bb96b2fbafd7c64b9fa251ca74
Honeybee4642 <tildenbruins@gmail.com>
2023-11-29 12:04:04 -0600

    Experimented with p5play

--- sketch.js
+++ sketch.js
@@ -5,17 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let blackhole;
+let star;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  blackhole = new Sprite();
+  blackhole = new Sprite(width/2, height/2, 50);
+  blackhole.color = (0,0,0);
 }
 
 function draw() {
   background(220);
-
 }
-
-function mousePressed() {
-  
+function attract(){
+  if star
 }
