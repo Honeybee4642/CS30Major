9e560a1fa5b9f12d000e71468f21311b3b31ae45
Honeybee4642 <tildenbruins@gmail.com>
2023-12-05 12:17:01 -0600

    Created Blackhole class

--- sketch.js
+++ sketch.js
@@ -5,15 +5,29 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let blackhole;
-let star;
+let star = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  blackhole = new Sprite(width/2, height/2, 50);
-  blackhole.color = (0,0,0);
-  star = new Sprite(mouseX, mouseY, random(3, 35));
+  blackhole = new Blackhole(windowWidth/2, windowHeight/2);
 }
 
 function draw() {
   background(220);
+  blackhole.display();
+}
+class Blackhole{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.size = 50;
+    this. outSize = this.size * 1.5;
+  }
+  display(){
+    noStroke();
+    fill(255, 125, 2);
+    circle(this.x , this.y, this.outSize);
+    fill(0,0,0);
+    circle(this.x, this.y, this.size);
+  }
 }
