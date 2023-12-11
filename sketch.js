747634dd537c05f24d66fdc403ef5d12722432f5
Honeybee4642 <tildenbruins@gmail.com>
2023-12-11 12:18:01 -0600

    Tried to make the Star spiral to blackhole but kept getting an error that says theObj is not imitable

--- sketch.js
+++ sketch.js
@@ -15,6 +15,15 @@ function setup() {
 function draw() {
   background(220);
   blackhole.display();
+  for(let stars of star){
+    stars.display();
+    blackhole.attract(stars);
+  }
+  
+}
+function mousePressed(){
+  let theStar = new Star(mouseX, mouseY);
+  star.push(theStar);
 }
 class Blackhole{
   constructor(x, y){
@@ -23,6 +32,8 @@ class Blackhole{
     this.size = 50;
     this. outSize = this.size * 1.5;
     this.pullDist = this.outSize * 3;
+    this.r = this.pullDist;
+    this.angle = 0;
   }
   display(){
     noStroke();
@@ -31,10 +42,24 @@ class Blackhole{
     fill(0,0,0);
     circle(this.x, this.y, this.size);
   }
+  attract(theObj){
+    for(let starDist of theObj){
+      if(this !== starDist){
+        if(dist(this.x, this.y , starDist.x, starDist.y) < this.pullDist){
+          star = this.r * cos(this.angle);
+          star = this.r * sin(this.angle);
+          point(this.x, this.y);
+          this.angle += 0.04;
+          this.r -= 2;
+        }
+      }
+    }
+  }
 }
-class Star extends Blackhole{
-  constructor(x, y, pullDist){
-    super(pullDist);
+
+class Star{
+  constructor(x, y){
+
     this.x = x;
     this.y = y;
     this.size = random(3, 40);
@@ -45,12 +70,5 @@ class Star extends Blackhole{
     fill(this.color);
     circle(this.x, this.y, this.size);
   }
-  update(theDist){
-    for(let starDist of theDist){
-      if(this !== theDist){
-        if(dist(this.x, this.y, starDist.x, starDist.y) < this.pullDist)
-        
-      }
-    }
-  }
+  
 }
